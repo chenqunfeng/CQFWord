5b75fcc901cbce29d653b79bd348eb986088df0b
e <604590172@qq.com>
2016-11-23 18:45:24 +0800

    fear: finish search box style

--- app/src/components/es6/app.js
+++ app/src/components/es6/app.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import menuBar from './../MenuBar.vue'
+import searchBox from './../SearchBox.vue'
 Vue.component('menu-bar', menuBar)
-export default {
-    data () {
-        return {
-            name:"cqf",
-            age:"22"
-        }
-    }
-}
+Vue.component('search-box', searchBox)
+// export default {
+//     data () {
+//         return {
+//             name:"cqf",
+//             age:"22"
+//         }
+//     }
+// }
